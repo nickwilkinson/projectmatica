cc5be6fdaad93b1d23d335a23edb078c8f402a75
Nick Wilkinson <nwilkinson@artefactual.com>
2017-12-28 14:24:58 -0800

    add environment-specific settings (base, local, production) stored in a new settings folder, where secrets are stored separately in an untracked env file

--- asites/wsgi.py
+++ asites/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+from os.path import abspath, dirname
+from sys import path
+
+SITE_ROOT = dirname(dirname(abspath(__file__)))
+path.append(SITE_ROOT)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "asites.settings.local")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "asites.settings")
-
 application = get_wsgi_application()
